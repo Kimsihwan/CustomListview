4ceddbae36e4d862958df4335e51a27e759c349f
Kimsihwan <namid882@gmail.com>
2018-04-14 01:52:06 +0900

    티스토리 완성본

---
+++ app/src/main/java/com/example/test/customlistview/ListViewItem.java
@@ -0,0 +1,38 @@
+package com.example.test.customlistview;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Test on 2018-04-14.
+ */
+
+public class ListViewItem {
+    private Drawable iconDrawable;
+    private String titleStr;
+    private String descStr;
+
+    public void setIconDrawable(Drawable iconDrawable) {
+
+        this.iconDrawable = iconDrawable;
+    }
+
+    public void setTitleStr(String titleStr) {
+        this.titleStr = titleStr;
+    }
+
+    public void setDescStr(String descStr) {
+        this.descStr = descStr;
+    }
+
+    public Drawable getIconDrawable() {
+        return iconDrawable;
+    }
+
+    public String getTitleStr() {
+        return titleStr;
+    }
+
+    public String getDescStr() {
+        return descStr;
+    }
+}
--- app/src/main/java/com/example/test/customlistview/MainActivity.java
+++ app/src/main/java/com/example/test/customlistview/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.test.customlistview;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listview;
+        ListViewAdapter adapter;
+
+        adapter = new ListViewAdapter();
+
+        listview = (ListView) findViewById(R.id.listView1);
+        listview.setAdapter(adapter);
+
+
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.image01), "첫번째사진", "첫번째내용");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.image02), "2번째사진", "2번째내용");
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.image03), "3번째사진", "3번째내용");
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+          ListViewItem item = (ListViewItem) adapterView.getItemAtPosition(i);
+
+          String titleStr = item.getTitleStr();
+          String descStr = item.getDescStr();
+                Drawable iconDrawable = item.getIconDrawable();
+            }
+        });
     }
 }
